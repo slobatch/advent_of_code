d2553c728b32960dd4ca6d39ab658bd283d82ab2
Serge Lobatch <serge@hireserge.com>
2022-12-01 15:10:13 -0800

    Add day 1 part 2 solution code

--- day1.py
+++ day1.py
@@ -22,10 +22,18 @@ def sum_each_list(list_of_lists):
         summed_list.append(sum(list))
     return summed_list
 
-
+# Part 1 solution
 elf_calorie_inventory = extract_data(input)
 summed_elf_calorie_inventory = sum_each_list(elf_calorie_inventory)
 max_elf_calories = max(summed_elf_calorie_inventory)
 
 print(max_elf_calories)
+
+# Part 2 solution
+sorted_summed_elf_calorie_inventory = sorted(summed_elf_calorie_inventory,reverse=True)
+top_three_elf_calories = sorted_summed_elf_calorie_inventory[:3]
+
+print(sum(top_three_elf_calories))
+
+
 
